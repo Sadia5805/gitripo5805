1056891a59c2f9a4af2a46df91d9cb9078faedda
Sadia5805 <tasnim15-5805@diu.edu.bd>
2025-03-26 14:21:39 +0600

    task 2

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:gitripo5805/main.dart';
+import 'package:gitripo5805/task2/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
